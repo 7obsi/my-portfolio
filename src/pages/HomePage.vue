4b6e61394bdd2095ec09e212a0544d69d9e523e3
Tobias Ellmauer <tobias.ellmauer@roiderhof.at>
2025-10-24 15:30:00 +0200

    add tailwind

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bg-green-500 text-white p-4 text-xl">
+    ✅ Tailwind works!
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
